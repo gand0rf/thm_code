23ac5ff303803a212117189eed1a2080f5e7c413
gand0rf <gand0rf@protonmail.com>
2024-06-24 10:37:41 -0400

    Fixed some typos

--- w1seguy/solution.py
+++ w1seguy/solution.py
@@ -1,17 +1,22 @@
 # text is for the string that the server gives you.
 text = '302e2722245507063720211e1e182010520932372508186a35082a1331011612136921161e252b29'
+
 # temp will hold the dehexed value of text
 temp = ''
+
 # key is going to store the key value that was used to encrypt the flag
 key = ''
+
 # key length given by the box
 key_len = 5
+
 # flag format specified by the answer format of the questions
 flag_k = 'THM{}'
+
 # variable to hold the unencrypted flag_1
 flag_1 = ''
 
-# The loop will go through and unhex the text string fro the server
+# The loop will go through and unhex the text string from the server
 for x in range(0,len(text),2):
     temp += bytearray.fromhex(text[x:x+2]).decode()
 
